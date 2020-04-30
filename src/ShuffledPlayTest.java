6b48de33c1a61233948c9323330347a5905fa517
maxim.soukharev <maxim.soukharev@gmail.com>
2020-04-29 21:03:46 -0400

    Doing 4, some crazy stuff happening

--- src/ShuffledPlayTest.java
+++ src/ShuffledPlayTest.java
@@ -1,25 +1,45 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.lang.reflect.*;
 
-public class ShuffledPlayTest {
+public class ShuffledOrderTest {
 
-    private final MusicPlayer player = new MusicPlayer();
-    private final Song[] SONGPOOL = {
-            new Song("Song1", "A", 1),
-            new Song("Song2", "A", 2),
-            new Song("Song3", "A", 3),
-            new Song("Song4", "A", 4),
-            new Song("Song5", "A", 5),
-            new Song("Song6", "A", 6),
+    final static Song[] SONGS = {
+            new Song("Song1", "1", 1),
+            new Song("Song2", "1", 1),
+            new Song("Song3", "1", 1),
+            new Song("Song4", "1", 1),
+            new Song("Song5", "1", 1),
+            new Song("Song6", "1", 1),
     };
 
+    private MusicPlayer player;
+
     @BeforeEach
-    private void initialize() {
-        /* Load songs into the app */
-        for (Song s : SONGPOOL) {
+    public void init() {
+        player = new MusicPlayer();
+        for (Song s : SONGS) {
             player.addItem(s);
         }
     }
 
+    @Test
+    public void hasNextText() throws NoSuchFieldException {
+        player.createPlaylist("pl1");
+        player.addSongToPlaylist("Song3", "pl1");
+        player.addSongToPlaylist("Song2", "pl1");
+        player.addSongToPlaylist("Song1", "pl1");
 
+        player.enqueuePlaylist("pl1");
+
+        PlayOrder order = player.new ShuffledPlay();
+
+        player.setupPlayOrder(order);
+
+        
+
+
+
+
+    }
 }
